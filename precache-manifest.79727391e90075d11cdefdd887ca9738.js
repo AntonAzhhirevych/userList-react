f8f794530c76d21b7d1dde73aabdb5c2c295420d
Anton Azhhirevych <azhhirevych943@gmail.com>
2019-09-22 16:28:26 +0300

    Updates

---
+++ precache-manifest.79727391e90075d11cdefdd887ca9738.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d0784e8c87622c76400e90bf1b0d26a0",
+    "url": "/venbest-test/index.html"
+  },
+  {
+    "revision": "7de5eec6e899d9c45ef3",
+    "url": "/venbest-test/static/css/2.21795a30.chunk.css"
+  },
+  {
+    "revision": "7de5eec6e899d9c45ef3",
+    "url": "/venbest-test/static/js/2.76424430.chunk.js"
+  },
+  {
+    "revision": "1f196bbcc32aa6164eb7",
+    "url": "/venbest-test/static/js/main.93feb397.chunk.js"
+  },
+  {
+    "revision": "1a67edb5c698b8ccd183",
+    "url": "/venbest-test/static/js/runtime-main.a5b322e0.js"
+  }
+]);
